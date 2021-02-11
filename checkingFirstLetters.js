0f8dd8a54bb4d81174f36d575309415181a647a0
Martyg <martinpaulgray@yahoo.co.uk>
2021-02-11 15:25:40 +0000

    test can be deleted

---
+++ checkingFirstLetters.js
@@ -0,0 +1,35 @@
+// Returns the string whose first letter is later in the alphabet. If both first letters are equal, returns null.
+function getLaterFirstLetter(string1, string2) {
+  const firstLetter1 = string1.charAt(0);
+  const firstLetter2 = string2.charAt(0);
+
+  if (firstLetter1 === firstLetter2) {
+    return null;
+  } else if (firstLetter1 < firstLetter2) {
+    return string2;
+  } else {
+    return string1;
+  }
+}
+
+// Should return "blueberry"
+console.log("getLaterFirstLetter('avocado', 'blueberry') returns: " + getLaterFirstLetter('avocado', 'blueberry'));
+
+// Should return "zebra"
+console.log("getLaterFirstLetter('zebra', 'aardvark') returns : " + getLaterFirstLetter('zebra', 'aardvark'));
+
+// Should return null
+console.log("getLaterFirstLetter('astro', 'afar') returns: " + getLaterFirstLetter('astro', 'afar'));
+
+
+// Returns whether or not the provided string contains a substring of "cake" in it.
+function containsCake(string) {
+  const substring = "cake";
+return (string.includes(substring));
+}
+
+// Should return true
+console.log("containsCake('I think cake is my soul mate.') returns: " + containsCake('I think cake is my soul mate.'));
+
+// Should return false
+console.log("containsCake('Pie is certainly the coolest dessert.') returns: " + containsCake('Pie is certainly the coolest dessert.'));
